07e36584e7b26790bc362c90e281e762539850fc
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-06-11 17:53:37 -0300

    fix: remove fullWidth prop

--- src/components/Formik/FormikTextFieldPassword/index.tsx
+++ src/components/Formik/FormikTextFieldPassword/index.tsx
@@ -1,10 +1,10 @@
 import { ComponentProps, memo } from 'react'
 import { useField, ErrorMessage } from 'formik'
 
-import { TextFieldPassword } from '@/src/components';
+import { TextFieldPassword } from '@/src/components'
 
 import * as Styles from '../styles'
-import { Flex } from '@/src/styles';
+import { Flex } from '@/src/styles'
 
 type RootTextFieldProps = ComponentProps<typeof TextFieldPassword>
 
@@ -12,21 +12,21 @@ interface FormikTextFieldPasswordProps extends RootTextFieldProps {
   name: string;
 }
 
-function BaseFormikTextFieldPassword ({ fullWidth, ...props}: FormikTextFieldPasswordProps) {
-  const [field, meta, helpers] = useField(props);
+function BaseFormikTextFieldPassword ({ ...props}: FormikTextFieldPasswordProps) {
+	const [field, meta, helpers] = useField(props)
 
-  return (
-    <Flex 
-      flexDirection="column" 
-      gap="xs" 
-      fullWidth
-    >
-      <TextFieldPassword {...field} {...props} />
-      <ErrorMessage name={props.name}>
-        {msg => <Styles.Error>{msg}</Styles.Error>}
-      </ErrorMessage>
-    </Flex>
-  )
+	return (
+		<Flex 
+			flexDirection="column" 
+			gap="xs" 
+			fullWidth
+		>
+			<TextFieldPassword {...field} {...props} />
+			<ErrorMessage name={props.name}>
+				{msg => <Styles.Error>{msg}</Styles.Error>}
+			</ErrorMessage>
+		</Flex>
+	)
 }
 
 export const FormikTextFieldPassword = memo(BaseFormikTextFieldPassword)
