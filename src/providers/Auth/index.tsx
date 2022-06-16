f2901d95b3764005c84aa8f3180a689e06ee9c84
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-06-16 14:52:55 -0300

    fix:(AuthContext): remove unused param

--- src/providers/Auth/index.tsx
+++ src/providers/Auth/index.tsx
@@ -4,8 +4,6 @@ import { createContext, ReactNode, useContext, useEffect } from 'react'
 
 import { auth } from '@/src/lib/firebase'
 import { signInAnonymously } from '@/src/lib/firebase/auth'
-import { deletePlayer } from '@/src/services/firebase'
-import { useParams } from 'react-router-dom'
 
 interface Auth {
   user: User
@@ -17,11 +15,7 @@ interface AuthProviderProps {
   children: ReactNode
 }
 export function AuthProvider ({ children }: AuthProviderProps) {
-	const { id: room_id } = useParams()
-	
-	const { user } = useAutState(auth, {
-		...(room_id && { onBeforeUserLogout: user => deletePlayer(room_id, user.uid) })
-	})
+	const { user } = useAutState(auth)
 
 	useEffect(() => {
 		if (!user) signInAnonymously()
